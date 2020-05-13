d999ab19c3bbeef00e9a079048411582f574c5ca
Ahmed94Dawy <65256207+Ahmed94Dawy@users.noreply.github.com>
2020-05-13 02:58:34 +0200

    Add files via upload

---
+++ Project_1/models.py
@@ -0,0 +1,93 @@
+## TODO: define the convolutional neural network architecture
+
+## TODO: define the convolutional neural network architecture
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# can use the below import should you choose to initialize the weights of your Net
+import torch.nn.init as I
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## 1. This network takes in a square (same width and height), grayscale image as input
+        ## 2. It ends with a linear layer that represents the keypoints
+        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        
+        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        '''
+         
+        self.conv1 = nn.Conv2d(1, 32, 5) #outsize= (92,92,32)
+        self.max_pool = nn.MaxPool2d(2,2) #outsize(46,46,32)
+        
+        self.conv2 = nn.Conv2d(32, 64, 5)#outsize_aft_pool=(21,21,64)
+
+        self.conv3 = nn.Conv2d(64, 128, 5)#outsize_aft_pool=(8,8,128)
+
+        self.conv4 = nn.Conv2d(128, 256, 5)#outsize_aft_pool=(2,2,256)
+        
+        self.fcn1 = nn.Linear((2*2*256) , 500)
+        
+        self.dropout =nn.Dropout(p=0.4)
+
+        self.fcn2 = nn.Linear(500,(68*2))
+        '''
+        
+         
+        self.conv1 = nn.Conv2d(1, 32, 5) #outsize= (92,92,32)
+        self.max_pool = nn.MaxPool2d(2,2) #outsize(46,46,32)
+        
+        self.conv2 = nn.Conv2d(32, 64, 5)#outsize_aft_pool=(21,21,64)
+
+        self.conv3 = nn.Conv2d(64, 128, 5)#outsize_aft_pool=(8,8,128)
+
+        self.conv4 = nn.Conv2d(128, 256, 5)#outsize_aft_pool=(2,2,256)
+        
+        self.fcn1 = nn.Linear((2*2*256) , 500)
+        
+        self.dropout =nn.Dropout(p=0.3)
+
+        self.fcn2 = nn.Linear(500,(68*2))
+
+                              
+
+
+        
+        ## Note that among the layers to add, consider including:
+        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        
+
+        
+    def forward(self, x):
+        ## TODO: Define the feedforward behavior of this model
+        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
+        x = self.max_pool(F.relu(self.conv1(x)))
+
+        
+        x = self.max_pool(F.relu(self.conv2(x)))
+
+        
+        x = self.max_pool(F.relu(self.conv3(x)))
+       
+        x = self.max_pool(F.relu(self.conv4(x)))
+
+        
+        x = x.view(x.size(0),-1)
+        
+        x = F.relu(self.fcn1(x))
+        
+        x = self.dropout(x)
+        
+        x = self.fcn2(x)
+
+
+
+        
+        # a modified x, having gone through all the layers of your model, should be returned
+        return x
